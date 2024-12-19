5eed42463399bd34f960493b44974fc6e970ea2b
Deepak Kumar <deepak.kumar1@phonepe.com>
2024-12-19 23:24:54 +0530

    added top dropdown to select different locations

--- mapApp-swiftUI/Models/Location.swift
+++ mapApp-swiftUI/Models/Location.swift
@@ -8,30 +8,22 @@
 import Foundation
 import MapKit
 
-struct Location: Identifiable {
+struct Location: Identifiable, Equatable {
+    
+    //Identifiable -> to uniquely identify in a list
     let id = UUID().uuidString
+    
     let name: String
     let cityName: String
     let coordinates: CLLocationCoordinate2D
     let description: String
     let imageNames: [String]
     let link: String
+    
+    //Equatable -> to know whether two instances of this are same or not, basically to tell on what basis it should be differentiated
+    static func == (lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
 }
 
 
-
-/*
- 
- Location(
-     name: "Colosseum",
-     cityName: "Rome",
-     coordinates: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922),
-     description: "The Colosseum is an oval amphitheatre in the centre of the city of Rome, Italy, just east of the Roman Forum. It is the largest ancient amphitheatre ever built, and is still the largest standing amphitheatre in the world today, despite its age.",
-     imageNames: [
-         "rome-colosseum-1",
-         "rome-colosseum-2",
-         "rome-colosseum-3",
-     ],
-     link: "https://en.wikipedia.org/wiki/Colosseum"),
- 
- */
--- mapApp-swiftUI/ViewModels/LocationsViewModel.swift
+++ mapApp-swiftUI/ViewModels/LocationsViewModel.swift
@@ -20,9 +20,12 @@ class LocationsViewModel: ObservableObject {
         }
     }
     
+    // current region on the map
     @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
-    
     let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
+    // show list of locations
+    @Published var showLocationsList: Bool = false
     
     init() {
         let locations = LocationsDataService.locations
@@ -39,4 +42,18 @@ class LocationsViewModel: ObservableObject {
                 span: mapSpan)
         }
     }
+    
+    func toggleLocationsList() {
+        withAnimation(.easeInOut) {
+            showLocationsList = !showLocationsList
+        }
+    }
+    
+    func showNextLocation(location: Location) {
+        withAnimation(.easeInOut) {
+            mapLocation = location
+            showLocationsList = false
+        }
+    }
+    
 }
